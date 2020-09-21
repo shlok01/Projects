7fc187ff2e620855d79d19f07350ba9d2dd06748
shftk3y <shlok0196@gmail.com>
2020-09-21 18:50:05 +0530

    change mongodb port no.

--- Yelpcamp/app.js
+++ Yelpcamp/app.js
@@ -18,7 +18,7 @@ var authRoutes       = require("./routes/auth");
 
 
 
-mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true , useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true , useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine" , "ejs");
 app.use(express.static(__dirname + "/public"));
