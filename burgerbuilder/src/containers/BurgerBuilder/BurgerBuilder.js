bc2d70aa1310c26931b6d4b109078207962bbc1f
shftk3y <shlok0196@gmail.com>
2020-09-22 19:55:29 +0530

    change Aux file path

--- burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React , { Component } from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
@@ -84,7 +85,7 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert("You continue!")
-        this.setState( { loading : true } );
+        this.setState( { loading: true } );
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -98,13 +99,13 @@ class BurgerBuilder extends Component {
             },
             deliveryMethod: 'fastest'
         }
-        // axios.post('/orders.json' , order)
-        // .then(response => {
-        //     this.setState( { loading: false, purchasing:false } )
-        // } )
-        // .catch( error => {
-        //     this.setState( { loading: false, purchasing:false } )
-        // } );
+        axios.post('/orders.json' , order)
+        .then(response => {
+            this.setState( { loading: false, purchasing:false } )
+        } )
+        .catch( error => {
+            this.setState( { loading: false, purchasing:false } )
+        } );
     }
 
     render() {
@@ -144,4 +145,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler (BurgerBuilder, axios);
